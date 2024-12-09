7c1da35784199507820b4f14db9e25cc8c5dce2d
henry <yuming.guo@advancegroup.com>
2024-12-09 11:38:14 +0800

    添加脚本退出功能和检测桌面App是否已经打开

--- connect-vpn.sh
+++ connect-vpn.sh
@@ -4,7 +4,55 @@ ANYCONNECT_CMD=/opt/cisco/anyconnect/bin/vpn
 CONN_INFO=(
 "VPN_ADDRESS USERNAME PASSWORD"
 "vpn.github.com:8843 zhangsan 123456"
+"Exit"
 )
+
+_exists() {
+    cmd="$1"
+    if [ -z "$cmd" ]; then
+        _usage "Usage: _exists cmd"
+        return 1
+    fi
+
+    if eval type type >/dev/null 2>&1; then
+        eval type "$cmd" >/dev/null 2>&1
+    elif command >/dev/null 2>&1; then
+        command -v "$cmd" >/dev/null 2>&1
+    else
+        which "$cmd" >/dev/null 2>&1
+    fi
+    ret="$?"
+    return $ret
+}
+
+_contains() {
+    _str="$1"
+    _sub="$2"
+    echo "$_str" | grep -- "$_sub" >/dev/null 2>&1
+}
+
+check_and_stop_anyconnect() {
+    if ! _exists pgrep ;then
+        echo "prgep command does not exist."
+        exit 1
+    fi
+
+    if ! _exists pkill ;then
+        echo "pkill command does not exist."
+        exit 1
+    fi
+
+	if pgrep -f "AnyConnect" > /dev/null; then
+		read -r -p "The AnyConnect Secure Mobility Client is running. Do you want to stop the desktop client? (y/N): "  response
+		if [[ "$response" == "y" || "$response" == "Y" ]]; then
+			pkill -f "AnyConnect"
+			echo "The desktop client has stopped."
+		else
+			echo "Please exit the desktop client before running this script"
+			exit 1
+		fi
+	fi
+}
 
 select_option() {
 
@@ -62,7 +110,25 @@ select_option() {
     cursor_blink_on
 
     return $selected
+
+    #echo "Select one option using up/down keys and enter to confirm:"
+    #echo
+    #
+    #options=("one" "two" "three")
+    #
+    #select_option "${options[@]}"
+    #choice=$?
+    #
+    #echo "Choosen index = $choice"
+    #echo "        value = ${options[$choice]}"
+
+    #case `select_opt "Yes" "No" "Cancel"` in
+    #    0) echo "selected Yes";;
+    #    1) echo "selected No";;
+    #    2) echo "selected Cancel";;
+    #esac
 }
+
 
 select_opt() {
     select_option "$@" 1>&2
@@ -71,35 +137,15 @@ select_opt() {
     return $result
 }
 
-_exists() {
-    cmd="$1"
-    if [ -z "$cmd" ]; then
-        _usage "Usage: _exists cmd"
-        return 1
-    fi
-
-    if eval type type >/dev/null 2>&1; then
-        eval type "$cmd" >/dev/null 2>&1
-    elif command >/dev/null 2>&1; then
-        command -v "$cmd" >/dev/null 2>&1
-    else
-        which "$cmd" >/dev/null 2>&1
-    fi
-    ret="$?"
-    return $ret
-}
-
-_contains() {
-    _str="$1"
-    _sub="$2"
-    echo "$_str" | grep -- "$_sub" >/dev/null 2>&1
-}
-
 connect() {
     options=($(echo ${CONN_INFO[@]%% *}))
     case `select_opt "${options[@]}"` in
       *)
         host=${options[$?]}
+        if [ "$host" == "Exit" ];then
+            echo "Exiting..."
+            exit 0
+        fi
         echo "connect $host"
         ;;
     esac
@@ -144,6 +190,8 @@ Commands:
 }
 
 main() {
+    check_and_stop_anyconnect
+
     if ! _exists "$ANYCONNECT_CMD";then
         echo "$ANYCONNECT_CMD not exist."
         exit 1
